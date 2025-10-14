fcdb959e6c885725100c5f7cef5a7254cb69beda
swaminathanmonika-commits <swaminathanmonika@gmail.com>
2025-10-14 14:51:50 +0530

    Add files via upload

---
+++ src/src/basics/UserInput.java
@@ -0,0 +1,12 @@
+package basics;
+public class UserInput{
+	public static void main(String[]args) {
+		char ch='A';
+		int val=(int)ch;
+		int op=ch;
+		System.out.println(val);
+		System.out.println(op);
+	}
+	    
+}
+
---
+++ src/src/basics/Varabiles.java
@@ -0,0 +1,8 @@
+package basics;
+import java.util.scanner;
+public class  UserInput{
+	public static void main(String[]args)
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("enter the input")
+
+}
